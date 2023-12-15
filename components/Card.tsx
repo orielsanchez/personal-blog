ec1d8bc01e1511c3520ed35de283d66207341b22
Oriel Sanchez <orielbenitosanchez@gmail.com>
2023-12-14 20:56:01 -0800

    removed images

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,33 +1,6 @@
-import Image from './Image'
 import Link from './Link'
 
-const Card = ({ title, description, imgSrc, href }) => (
-  <div className="md max-w-[544px] p-4 md:w-1/2">
-    <div
-      className={`${
-        imgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
-    >
-      {imgSrc &&
-        (href ? (
-          <Link href={href} aria-label={`Link to ${title}`}>
-            <Image
-              alt={title}
-              src={imgSrc}
-              className="object-cover object-center md:h-36 lg:h-48"
-              width={544}
-              height={306}
-            />
-          </Link>
-        ) : (
-          <Image
-            alt={title}
-            src={imgSrc}
-            className="object-cover object-center md:h-36 lg:h-48"
-            width={544}
-            height={306}
-          />
-        ))}
+const Card = ({ title, description, href }) => (
       <div className="p-6">
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
           {href ? (
@@ -49,8 +22,6 @@ const Card = ({ title, description, imgSrc, href }) => (
           </Link>
         )}
       </div>
-    </div>
-  </div>
 )
 
 export default Card
