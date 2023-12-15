5d1a1a26b84616728121aef2e347883968e3ab28
Oriel Sanchez <orielbenitosanchez@gmail.com>
2023-12-14 20:50:26 -0800

    fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -18,15 +18,14 @@ export default function Projects() {
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
-            {projectsData.map((d) => {
-                return (
-                    <Card
-                        key={d.title}
-                        title={d.title}
-                        description={d.description}
-                        href={d.href} />
-                )
-            })}
+            {projectsData.map((d) => (
+              <Card
+                key={d.title}
+                title={d.title}
+                description={d.description}
+                href={d.href}
+              />
+            ))}
           </div>
         </div>
       </div>
