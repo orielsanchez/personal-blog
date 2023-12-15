7149bac8ad8b736cfed39a13a063c3e5e81fcbbc
Oriel Sanchez <orielbenitosanchez@gmail.com>
2023-12-14 21:21:33 -0800

    check

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,6 +12,9 @@ export default function Projects() {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Projects
           </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          More to come!
+           </p>
         </div>
         <div className="container py-12">
           <div className="-m-4 flex flex-wrap">
